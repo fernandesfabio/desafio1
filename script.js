4602f8f97db57d58e24ccfb96e2251c59c5e7373
Fabio da Silva Fernandes <fernandesfabio@fabios-MacBook-Air.local>
2023-12-19 23:09:16 -0300

    desafio02

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let name = 'Fabio';
-let lastName = 'Fernandes'
-let space = ' ';
-let year = 2023;
-let age = 33;
 
-let fullName = name+space+lastName;
-let yearBorn = year-age;
+function insiraPrecoCusto() {
+    const precoCusto = parseFloat(prompt('Insira o valor do produto:'))
+    
+    return precoCusto
+}
 
-console.log("Seja Bem vindo!"+space+fullName+space+"você nasceu em:"+space+yearBorn);
+function somaICMS() {
+    const taxaICMS = 0.21
+    const valorICMS = precoCusto * taxaICMS
+    return valorICMS
+}
 
-let valor1String = prompt('Insira o primeiro número:');
-let valor2String = prompt('Insira o segundo número:');
-
-let valor1 = parseInt(valor1String);
-let valor2 = parseInt(valor2String);
-
-let valorTotal = valor1+valor2;
-
-console.log('O resultado da soma dos números é de :'+valorTotal);
-alert('O resultado da soma dos números é de :'+space+valorTotal);
+function mostrar(precoCusto, valorICMS) {
+    const precoFinal = precoCusto + valorICMS
+    alert('Preço final com ICMS: R$' + precoFinal)
+}
+const precoCusto = insiraPrecoCusto()
+const valorICMS = somaICMS()
+mostrar(precoCusto, valorICMS)
